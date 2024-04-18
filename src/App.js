ca0a6ac71fe364c7ed98878a8fb5a9bf782d8c7f
juf468 <julia.franchi7@gmail.com>
2024-04-18 16:27:24 -0300

    global styles ready

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
+import GlobalStyles from "./styles.css/GlobalStyles";
 
 function App() {
   return (
-    <div className="App">
-      <h1> MeRcAdO LiBrE</h1>
+    <div >
+       <h1> MeRcAdO LiBrE</h1>
+       <GlobalStyles/> 
+       {/* {todos los components hijos de app tendran estos estilos} */}
     </div>
+     
+    
+    
   );
 }
 
---
+++ src/styles.css/GlobalStyles.js
@@ -0,0 +1,51 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle `
+:root{
+--fondo: #ededed;
+-- black: #000;
+-- withe: #fff;
+
+--text1: rgba(0,0,0,.8);
+--text2: rgba(0,0,0,.45);
+--text3: #666;
+
+--green: #00a650;
+--blue1: #1e6dff;
+--blue2: #2968c8;
+--yellow: #fff159;
+
+--border: #ddd;
+
+--panel: 0 1px 3px 0 rgba(0, 0, 0, .3)
+
+--reputacion1: #ffe7e6;
+--reputacion1: #fff4e7;
+--reputacion1: #fffde5;
+--reputacion1: #f3fee0;
+--reputacion1: #00a650;
+
+--family: "Proxima Nova" -apple-system, "Helvetica Neue", Helvetica,
+Roboto, Arial, sans-serif;
+}
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+h1, h2, h3, h4, h5, h6{
+    color: var(--text1);
+    font-weight: inherit;
+}
+html{
+    min-height: 100%;
+    background-color: vare(--fondo);
+}
+
+*, button, input{
+    border: 0;
+    background-color: none; 
+    font-family: var(--family)
+
+}
+`
