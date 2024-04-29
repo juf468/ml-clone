b0d169f3f63563457260fe642f3bae082099d40e
juf468 <julia.franchi7@gmail.com>
2024-04-29 12:19:34 -0300

    component Layout created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import GlobalStyles from "./styles.css/GlobalStyles";
+import {Layout} from "./Components/Layout";
 
 function App() {
   return (
     <div >
-       <h1> MeRcAdO LiBrE</h1>
+       <Layout/>
        <GlobalStyles/> 
        {/* {todos los components hijos de app tendran estos estilos} */}
     </div>
---
+++ src/Components/Layout/index.js
@@ -0,0 +1,10 @@
+
+import React from "react";
+
+export const Layout = () =>{
+    return(
+        <div>
+            <h1>Layouts</h1>
+        </div>
+    )
+}
